2e9cfb4ca075918e2fd2010449cc0d0c98fd2a76
parthsrivastava801 <parthsrivastava801@gmail.com>
2025-02-12 18:22:14 +0530

    correcting mistake

--- bus/bus/settings.py
+++ bus/bus/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'dashboard'  # to redirect user after a successful login
+LOGOUT_REDIRECT_URL = 'login'  # to redirect after a logout
+
